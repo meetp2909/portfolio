290f7a3299efacc965699d5269fda419370f791e
Meet <190120116069@git.org.in>
2021-11-27 21:59:01 +0530

    Initial commit

--- js/style.js
+++ js/style.js
@@ -1,5 +1,24 @@
+const closer = document.querySelector("#closer");
+
+    closer.onclick = () =>{
+        closer.style.display = "none";
+        drawer.classList.remove("active");
+        
+    }
+
+
+
+
+
+
 const drawer = document.querySelector(".drawer");
 
 document.querySelector("#menu").onclick = () =>{
     drawer.classList.toggle("active");
+    closer.style.display = "block";
 }
+
+
+
+
+
