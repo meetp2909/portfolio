08064d81e4838587ca3269fc64c88dc9b13f3df2
Meet <190120116069@git.org.in>
2021-12-02 00:08:19 +0530

    Initial commit

--- app.js
+++ app.js
@@ -15,11 +15,12 @@ app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 app.use('img', express.static(__dirname + 'public/img'))
 
-app.get("/", (req, res) =>{
+app.get("/#home", (req, res) =>{
     
     const data = fs.readFileSync("index.html")
     res.end(data.toString());
 })
+
 
 
 
